6021338cc1f423e7a98fd4a7c6025b17de29578e
NeginSal <neginsaljooghi@gmail.com>
2023-06-13 16:54:35 +0330

    send html pages to browser

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
-  console.log('request made');
+
   //set header content type
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello,ninjas');
-  res.end();
+  res.setHeader('Content-Type', 'text/html');
+
+  //send a html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.end();
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  })
 });
 
 server.listen(3000, 'localhost', () => {
