c660d949c1b3e903aeb6e7281b7d30feaad7e9e7
NeginSal <neginsaljooghi@gmail.com>
2023-06-22 15:11:02 +0330

    added:ejs

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ const express = require('express');
 const app = express();
 
 // listen for requests
-app.listen(3001);
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });
